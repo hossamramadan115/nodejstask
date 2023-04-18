cea601d4d0b63dce06df941e6670183a90bf53d7
hossamramadan115 <119107124+hossamramadan115@users.noreply.github.com>
2023-04-18 16:03:35 +0200

    Add files via upload

---
+++ database/info.js
@@ -0,0 +1,28 @@
+export const info = [
+    {
+        id: 1,
+        name: "hossam",
+        age: 19,
+    },
+    {
+        id: 2,
+        name: "ramadan",
+        age: 18,
+    },
+    {
+        id: 3,
+        name: "abdelhamed",
+        age: 20,
+    },
+    {
+        id: 4,
+        name: "waled",
+        age: 22,
+    },
+    {
+        id: 5,
+        name: "mohamed",
+        age: 18,
+    },
+];
+
